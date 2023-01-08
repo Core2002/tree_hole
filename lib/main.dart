395d1d906f08c1992ffa9d20b3b11491aa82abe5
NekokeCore <Core2002@163.com>
2023-01-08 08:04:11 +0800

    修复了同条数据重复请求的bug

--- lib/main.dart
+++ lib/main.dart
@@ -29,9 +29,9 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-Future<MESSAGE> getMESSAGE() async {
-  final response =
-      await http.get(Uri.parse('http://192.168.1.3:8080/api/random'));
+Future<MESSAGE> getMESSAGE(index) async {
+  final response = await http.get(Uri.parse(
+      'http://192.168.1.3:8080/api/read_index?hole=core&index=$index'));
 
   if (response.statusCode == 200) {
     Utf8Decoder utf8decoder = const Utf8Decoder();
@@ -48,14 +48,14 @@ class MESSAGE {
   final String hole;
   final String message;
   final int like;
-  final String time;
+  final String date;
   final String ip;
 
   const MESSAGE({
     required this.hole,
     required this.message,
     required this.like,
-    required this.time,
+    required this.date,
     required this.ip,
   });
 
@@ -79,7 +79,7 @@ class MESSAGE {
       hole: json['hole'],
       message: json['message'],
       like: json['like'],
-      time: json['date'],
+      date: json['date'],
       ip: json['ip'],
     );
   }
@@ -96,46 +96,53 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    var cache = {};
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
         toolbarHeight: 36,
       ),
-      body: ListView.builder(
-        itemBuilder: ((context, index) => Container(
-              height: 132,
-              padding: const EdgeInsets.all(18),
-              margin: const EdgeInsets.fromLTRB(18, 6, 18, 6),
-              decoration: const BoxDecoration(
-                color: Color.fromARGB(255, 198, 228, 255),
-                borderRadius: BorderRadius.all(Radius.circular(18)),
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 2,
-                    offset: Offset(0, 2),
-                    color: Colors.blue,
-                  )
-                ],
-              ),
-              child: FutureBuilder<MESSAGE>(
-                future: getMESSAGE(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    return Text(
-                      "${snapshot.data!.message}\n赞：${snapshot.data!.like}\nIP地址：${snapshot.data!.ip}",
-                      style: const TextStyle(
-                        fontSize: 18,
-                      ),
-                    );
-                  } else if (snapshot.hasError) {
-                    return Text('${snapshot.error}');
-                  }
-                  // By default, show a loading spinner.
-                  return const CircularProgressIndicator();
-                },
-              ),
-            )),
-      ),
+      body: ListView.builder(itemBuilder: ((context, index) {
+        if (!cache.containsKey(index)) {
+          cache[index] = getMESSAGE(index);
+        }
+
+        var card = FutureBuilder<MESSAGE>(
+          future: cache[index],
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Container(
+                height: 132,
+                padding: const EdgeInsets.all(18),
+                margin: const EdgeInsets.fromLTRB(18, 6, 18, 6),
+                decoration: const BoxDecoration(
+                  color: Color.fromARGB(255, 198, 228, 255),
+                  borderRadius: BorderRadius.all(Radius.circular(18)),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 2,
+                      offset: Offset(0, 2),
+                      color: Colors.blue,
+                    )
+                  ],
+                ),
+                child: Text(
+                  "${snapshot.data!.message}\n赞：${snapshot.data!.like}\nIP地址：${snapshot.data!.ip}",
+                  style: const TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+              );
+            } else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            }
+            // By default, show a loading spinner.
+            return const CircularProgressIndicator();
+          },
+        );
+        
+        return card;
+      })),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
