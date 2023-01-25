0a847142ce1660f84c289acf3a6cea2731b390cd
NekokeCore <Core2002@163.com>
2023-01-26 03:08:20 +0800

    添加HoleSendMessage

---
+++ lib/pojo/resp_send_message.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class RespSendMessage {
+  final int ok;
+
+  const RespSendMessage({required this.ok});
+
+  factory RespSendMessage.fromJson(Map<String, dynamic> json) {
+    return RespSendMessage(ok: json['res'] == "200 ok" ? 1 : -1);
+  }
+}
+
+Future<RespSendMessage> HoleSendMessage(String message) async {
+  final resp = await http.get(Uri.parse('http://192.168.1.3:8080/api/add_hole_message/core/$message'));
+  return RespSendMessage.fromJson(jsonDecode(resp.body));
+}
